0fa2a61779c8ce76140f8c06a9452ec511447d97
Ian Hirschfeld <ian@mightybell.com>
2014-03-17 23:19:06 -0700

    setsup unicorn and foreman, sets up js and css, makes static controller

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
+//= require vendor/modernizr
+//= require foundation
 //= require_tree .
+
+$(function(){ $(document).foundation(); });
